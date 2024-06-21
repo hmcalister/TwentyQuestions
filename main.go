e164df6a8ac4fea7fd40a8782c66c904268f4847
Hayden McAlister <haydenmcalister49@gmail.com>
2024-06-21 21:58:52 +1200

    Add router listen and serve

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
 var (
 	indexTemplate    = template.Must(template.New("index.html").ParseFiles("templates/index.html"))
 )
@@ -16,4 +21,19 @@ func main() {
 	debugFlag := flag.Bool("debug", false, "Flag for debug level with console log outputs.")
 	flag.Parse()
 
+	// --------------------------------------------------------------------------------
+	// Router and Middlewares
+	// --------------------------------------------------------------------------------
+
+	router := chi.NewRouter()
+	// --------------------------------------------------------------------------------
+	// Serve
+	// --------------------------------------------------------------------------------
+
+	targetBindAddress := fmt.Sprintf("localhost:%v", *port)
+	log.Info().Msgf("Starting server on %v", targetBindAddress)
+	err := http.ListenAndServe(targetBindAddress, router)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error during http listen and serve")
+	}
 }
