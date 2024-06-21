8526324c76131f7a2072429cd52231c28d52cff2
Hayden McAlister <haydenmcalister49@gmail.com>
2024-06-21 21:59:13 +1200

    Add zerolog and lumberjack setup

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+
 var (
 	indexTemplate    = template.Must(template.New("index.html").ParseFiles("templates/index.html"))
 )
@@ -20,6 +24,31 @@ func main() {
 	port := flag.Int("port", 3000, "The port to use for the HTTP server.")
 	debugFlag := flag.Bool("debug", false, "Flag for debug level with console log outputs.")
 	flag.Parse()
+
+	// --------------------------------------------------------------------------------
+	// Logging Setup
+	// --------------------------------------------------------------------------------
+
+	logFileHandle := &lumberjack.Logger{
+		Filename: "./logs/log",
+		MaxSize:  100,
+		MaxAge:   31,
+		Compress: true,
+	}
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.
+		With().Caller().Logger().
+		With().Timestamp().Logger()
+
+	log.Logger = log.Output(logFileHandle)
+	if *debugFlag {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+		consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+		multiWriter := zerolog.MultiLevelWriter(consoleWriter, logFileHandle)
+		log.Logger = log.Output(multiWriter)
+	}
 
 	// --------------------------------------------------------------------------------
 	// Router and Middlewares
