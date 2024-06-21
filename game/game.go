4d700c32d0cdb2823bcead0121ffe9217c38c4bc
Hayden McAlister <haydenmcalister49@gmail.com>
2024-06-22 02:02:16 +1200

    Add templates and gameData struct

--- game/game.go
+++ game/game.go
@@ -1,5 +1,23 @@
 package game
 
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	gameBaseTemplate = template.Must(template.New("gameBase.html").ParseFiles("templates/gameBase.html"))
+	gameItemTemplate = template.Must(template.New("gameItem.html").ParseFiles("templates/gameItem.html"))
+)
+
 type gameData struct {
+	GameID       string
+	router       *chi.Mux
+	numResponses int
+}
+
 	GameID string
 }
